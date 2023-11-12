254011cb146f3becc0d18c61b90ef89b1a4f9c80
Dario Sechi <dario.sechi.rnd@gmail.com>
2023-11-12 01:07:06 +0100

    Publish smaller more specific packages for each major variant

--- scripts/generate-exports.mjs
+++ scripts/generate-exports.mjs
@@ -4,6 +4,21 @@ import { cpus } from 'os'
 import Queue from 'queue'
 
 /**
+ * Arguments
+ */
+const TYPE = process.argv[2]
+const OS = process.argv[3]
+const FILL = process.argv[4] === 'fill'
+
+if (TYPE !== 'materialsymbolsrounded' && TYPE !== 'materialsymbolssharp') {
+  throw new Error(`The ${TYPE} type is not supported.`)
+}
+
+if (OS !== '20' && OS !== '24') {
+  throw new Error(`The ${OS} optical size is not supported.`)
+}
+
+/**
  * Constants
  */
 const ASSETS = await readdir('assets')
@@ -31,6 +46,10 @@ ASSETS.map((name) => {
       types.map(async (type) => {
         let svgs
 
+        if (TYPE !== type) {
+          return
+        }
+
         svgs = await readdir(`assets/${name}/${type}`)
         ScriptLogger.verbose(`The svgs are:`, svgs)
 
@@ -42,7 +61,11 @@ ASSETS.map((name) => {
             os = svg.match(/(20|24)px/)?.[0]?.replace('px', '')
             weight = svg.match(/wght[0-9]{3}/)?.[0]?.replace('wght', '') ?? 400
 
-            cname = `ICON_MATERIAL_SYMBOLS_${name.toUpperCase()}_${type.replace('materialsymbols', '').toUpperCase()}_OS${os}_W${weight}${fill ? '_F' : ''}`
+            if (FILL !== fill || OS !== os) {
+              return
+            }
+
+            cname = `ICON_MS_${name.toUpperCase()}_W${weight}`
             content = await readFile(`assets/${name}/${type}/${svg}`, 'utf8')
 
             if (!content.toLowerCase().includes('viewbox')) {
---
+++ scripts/publish.mjs
@@ -0,0 +1,39 @@
+import { Logger, getKebabCaseString } from '@aracna/core'
+import { execSync } from 'child_process'
+import { writeFile } from 'fs/promises'
+import PACKAGE_JSON from '../package.json' assert { type: 'json' }
+
+/**
+ * Constants
+ */
+const PACKAGES = [
+  'materialsymbolsrounded 20',
+  'materialsymbolsrounded 20 fill',
+  'materialsymbolsrounded 24',
+  'materialsymbolsrounded 24 fill',
+  'materialsymbolssharp 20',
+  'materialsymbolssharp 20 fill',
+  'materialsymbolssharp 24',
+  'materialsymbolssharp 24 fill'
+]
+
+/**
+ * Loggers
+ */
+const ScriptLogger = new Logger('ScriptLogger', 'info')
+
+for (let args of PACKAGES) {
+  let name
+
+  execSync(`node scripts/generate-exports.mjs ${args} && yarn build && cp LICENSE README.md dist`, { stdio: 'inherit' })
+  ScriptLogger.info(`The package has been built and packaged.`)
+
+  name = PACKAGE_JSON.name + '-' + getKebabCaseString(args.replace('materialsymbols', '').replace('20', 'os20').replace('24', 'os24'))
+  ScriptLogger.info(`The package name is ${name}.`)
+
+  await writeFile('dist/package.json', JSON.stringify({ ...PACKAGE_JSON, name }, null, 2))
+  ScriptLogger.info(`The package.json file has been written.`)
+
+  execSync(`cd dist && yarn pack && yarn publish *.tgz --access public --non-interactive`, { stdio: 'inherit' })
+  ScriptLogger.info(`The package ${name} has been packed and published.`)
+}
